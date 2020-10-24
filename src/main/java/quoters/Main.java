857e49371e568c3812d986e1440ded88f4eab7e5
Corneliu <corneliu.josan@yahoo.com>
2020-10-24 17:40:19 +0300

    Added Profiling BPP and MBean

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -3,8 +3,15 @@ package quoters;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-		context.getBean(TerminatorQuoter.class).sayQuote();
+		while (true) {
+			Thread.sleep(100);
+			context.getBean(Quoter.class).sayQuote();
+
+			// Get bean by name option
+//			Quoter terminatorQuoter = (Quoter) context.getBean("terminatorQuoter");
+//			terminatorQuoter.sayQuote();
+		}
 	}
 }
